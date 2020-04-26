579f44e3c5a645435b8bdcae3dc1c9b3c57f5b2c
sajad cj <sajad cj@192.168.1.198>
2020-04-26 01:50:27 -0700

    first part

---
+++ sahad's part/src/sahad/s/part/SahadSPart.java
@@ -0,0 +1,17 @@
+
+package sahad.s.part;
+
+/**
+ *
+ * @author sajad cj
+ */
+public class SahadSPart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
