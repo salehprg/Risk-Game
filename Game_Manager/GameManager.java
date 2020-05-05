090f6762532de0d12e5adf2e25af5da636faa328
saleh_prg <salehebrahimian@gmail.com>
2020-05-06 01:41:58 +0430

    UpdateSoldierManager

--- Game_Manager/GameManager.java
+++ Game_Manager/GameManager.java
@@ -1,4 +1,4 @@
-import Game_Manager.Turn.WarManger;
+
 
 public class GameManager {
 
--- Game_Manager/Turn/SoldierManager.java
+++ Game_Manager/Turn/SoldierManager.java
@@ -4,6 +4,14 @@ import PlayerManager.*;
 import Map.*;
 
 public class SoldierManager {
+
+    void GiveSoldierToPlayer(Player _Player)
+    {
+        _Player.UnimployedSoldiersCount += SoldiersByContinent(_Player) + SoldiersByCountries(_Player);
+    }
+
+
+
     int SoldiersByCountries(Player _Player)
     {
         return _Player.CountriesCount / 3;
@@ -12,14 +20,30 @@ public class SoldierManager {
     int SoldiersByContinent(Player _Player)
     {
         Continent _continents[] = Map.continents;
+        boolean HaveContinent = true;
+        int result = 0;
 
-        for(int i =0; i < _continents.length; i++)
+        
+        for(int i = 0; i < _continents.length; i++)
         {
+            HaveContinent = true;
+
             Continent CurrentContinent = _continents[i];
-            for(int j = 0; j < _continents[i].CountriesID.length; j++)
+            for(int j = 0; j < CurrentContinent.CountriesID.length; j++)
             {
-                
+                if(Map.getCountry(CurrentContinent.CountriesID[j]).ownerID != _Player.ID)
+                {
+                    HaveContinent = false;
+                    break;
+                }
+            }
+
+            if(HaveContinent)
+            {
+                result += CurrentContinent.Bountysoldier;
             }
         }
+
+        return result;
     }
 }
