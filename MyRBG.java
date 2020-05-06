5596206f958acfcff2bc0d924da1e9a7f3195aee
shafaf <sajadshafafq@gmail.com>
2020-05-06 03:15:11 -0700

    test2

--- MyRBG.java
+++ MyRBG.java
@@ -4,15 +4,11 @@ import PlayerManager.Player;
 public class MyRBG {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         Player players[] = new Player[4];
 
         GameManager gameManager = new GameManager(players);
         
         gameManager.InitializeGame();
-=======
-        //Test
->>>>>>> 643eb65df0890c0ace517fe814d6197cd550db7b
     }
     
 }
