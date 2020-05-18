c92bfe06018b1e0a69228a94a57f5d4a90b12b43
saleh_prg <salehebrahimian@gmail.com>
2020-05-19 02:55:45 +0430

    Basic Dialog For Deploy Soldier

--- Map/Continent.java
+++ Map/Continent.java
@@ -1,10 +1,12 @@
 package Map;
 
+import java.util.*;
+
 public class Continent 
 {   
     public String ContinentName ;//اسم هر قاره
     public int ContinentID ;//ID هر قاره
-    public int CountriesID[] ;//کشور های هر قاه ID 
+    public List<Integer> CountriesID = new ArrayList<Integer>();//کشور های هر قاه ID 
     public int Bountysoldier ; // تعداد سرباز اضافه در صورت تصاحب کامل هر قاره
     
 }
