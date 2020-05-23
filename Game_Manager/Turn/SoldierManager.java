f7d249dd906e73b5b9e79aabc24ef5dcba3f89f6
shafaf <sajadshafafq@gmail.com>
2020-05-23 10:44:02 -0700

    edit part 2

--- Game_Manager/Turn/SoldierManager.java
+++ Game_Manager/Turn/SoldierManager.java
@@ -63,13 +63,13 @@ public class SoldierManager {
     public static void Initialize(int n)
     {
         int [] CountryID = new int [41] ;//ID player owner dar khane haye arraye gharar migirnd
-        int limit = 41 / n ;//had aghal tedad country baraye har player
+        int limit = 42 / n ;//had aghal tedad country baraye har player
         int testCountry ;
         Random myRandom = new Random();
         int  remaining = 0 ;
         int [] remainingSoldierPlayer = new int [n + 1] ;//meghdar sarbaz baghi mande baad az taghsim countris
         
-        for(int i = 0 ; i < 41 ; i++)
+        for(int i = 0 ; i < 42 ; i++)
         {
             CountryID[i] = 0 ;
         }//meghdar 0 baraye tamam country ha
@@ -81,7 +81,7 @@ public class SoldierManager {
             
             while( limit >= CountryCount )
             {
-                testCountry = myRandom.nextInt(41) ;
+                testCountry = myRandom.nextInt(42) ;
                 if(CountryID[testCountry] == 0)//saheb nadasht
                 {
                     CountryID[testCountry] = i + 1 ;//player i+1 owner jadid mishavad
@@ -90,7 +90,7 @@ public class SoldierManager {
             }    
         }
 
-        for(int i =0 , j = 0; i < 41 ; i++)//peyda kardan owner country haye baghi monde
+        for(int i =0 , j = 0; i < 42 ; i++)//peyda kardan owner country haye baghi monde
         {
             if(CountryID[i] == 0)
             {
